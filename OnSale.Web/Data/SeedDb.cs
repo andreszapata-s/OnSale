254cdcdc04d0d2fea41e597b2a46ed9150772a52
andreszapata-s <andreszapata9804@gmail.com>
2020-11-25 10:24:45 -0500

    Add categories controller

---
+++ OnSale.Web/Data/SeedDb.cs
@@ -0,0 +1,94 @@
+﻿using OnSale.Common.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context; // Para que la inyeccion de dependencia funcione, se crea el context como un campo 
+
+        public SeedDb(DataContext context) //Cuando yo quiero hacer una conexion a la base de datos en cualquier clase, en el constructor agrego el datacontext
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync() // Metodo asincrono que pobla la base de datos
+        {
+            await _context.Database.EnsureCreatedAsync();// Esta instruccion si la base de datos no esta creado, esta la crea
+            await CheckCountriesAsync();//Garantiza que existan campos en la base de datos
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country
+                {
+                    Name = "Colombia",
+                    Departments = new List<Department>
+                {
+                    new Department
+                    {
+                        Name = "Antioquia",
+                        Cities = new List<City>
+                        {
+                            new City { Name = "Medellín" },
+                            new City { Name = "Envigado" },
+                            new City { Name = "Itagüí" }
+                        }
+                    },
+                    new Department
+                    {
+                        Name = "Bogotá",
+                        Cities = new List<City>
+                        {
+                            new City { Name = "Bogotá" }
+                        }
+                    },
+                    new Department
+                    {
+                        Name = "Valle del Cauca",
+                        Cities = new List<City>
+                        {
+                            new City { Name = "Calí" },
+                            new City { Name = "Buenaventura" },
+                            new City { Name = "Palmira" }
+                        }
+                    }
+                }
+                });
+                _context.Countries.Add(new Country
+                {
+                    Name = "USA",
+                    Departments = new List<Department>
+                {
+                    new Department
+                    {
+                        Name = "California",
+                        Cities = new List<City>
+                        {
+                            new City { Name = "Los Angeles" },
+                            new City { Name = "San Diego" },
+                            new City { Name = "San Francisco" }
+                        }
+                    },
+                    new Department
+                    {
+                        Name = "Illinois",
+                        Cities = new List<City>
+                        {
+                            new City { Name = "Chicago" },
+                            new City { Name = "Springfield" }
+                        }
+                    }
+                }
+                });
+                await _context.SaveChangesAsync();//Guardar datos en la base de datos
+            }
+        }
+
+    }
+}
+
